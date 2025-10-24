bb5742aab87e3180cddbca19bb8b56a15097326e
ismaeeeelshaikh <shaikhismaeel75123@gmail.com>
2025-10-24 19:38:26 +0530

    done2

--- app/api/admin/orders/route.js
+++ app/api/admin/orders/route.js
@@ -5,8 +5,6 @@ import Order from '@/lib/models/Order';
 export async function GET(request) {
   await connectDB();
   const orders = await Order.find({});
-
-  // Is line ko likhna ZARURI hai:
   const plainOrders = JSON.parse(JSON.stringify(orders));
   return NextResponse.json({ orders: plainOrders });
 }
